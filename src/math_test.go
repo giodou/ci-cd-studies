ac4c46092858c41098f33385c17820a25f1ace12
Giovane <giovane.negocios@gmail.com>
2022-10-12 10:40:27 -0300

    feat: added subtraction function and test

--- src/math_test.go
+++ src/math_test.go
@@ -9,3 +9,11 @@ func TestSoma(t *testing.T){
 		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
+
+func TestSubtracao(t *testing.T){
+	total := Subtracao(15,5)
+	
+	if total != 10 {
+		t.Errorf("Resultado da Subtracao é inválido: Resultado %d. Esperado: %d", total, 30)
+	}
+}
